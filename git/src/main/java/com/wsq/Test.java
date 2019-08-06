97057d51d96e902057aa7754316a13e0b45b595c
V47V47 <1522062224@qq.com>
2019-08-06 22:31:09 +0800

    3 推送

--- git/src/main/java/com/wsq/Test.java
+++ git/src/main/java/com/wsq/Test.java
@@ -8,5 +8,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
