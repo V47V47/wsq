519da804fdf8e0d140c65344712f8840fbb26468
V47V47 <1522062224@qq.com>
2019-08-07 00:12:01 +0800

    5 提交

--- git/src/main/java/com/wsq/Test.java
+++ git/src/main/java/com/wsq/Test.java
@@ -8,6 +8,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("提交github");
     }
 }
