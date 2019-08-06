9d4e679083b4408d84de3309ec171b74cf773b7d
V47V47 <1522062224@qq.com>
2019-08-06 22:25:46 +0800

    1 推送

---
+++ git/src/main/java/com/wsq/Test.java
@@ -0,0 +1,11 @@
+package com.wsq;
+
+/**
+ * @author: wsq
+ * @date:2019-08-06
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
