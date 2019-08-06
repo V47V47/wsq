04f1dab1d7be408ff5d93de849bd246798e623d9
V47V47 <1522062224@qq.com>
2019-08-07 00:15:39 +0800

    7 修改

--- git/src/main/java/com/wsq/Test.java
+++ git/src/main/java/com/wsq/Test.java
@@ -8,10 +8,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world");
-<<<<<<< HEAD
-        System.out.println("�޸�");
-=======
         System.out.println("提交github");
->>>>>>> 519da804fdf8e0d140c65344712f8840fbb26468
+        System.out.println("修改之后");
     }
 }
